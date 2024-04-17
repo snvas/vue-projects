2a2d4c5cfd49bce2d4e911c17f2fecf0008a237d
Soraia Novaes <sonovaes511@gmail.com>
2024-04-16 23:05:49 -0300

    10 Vue projects

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import TodoList from './components/TodoList.vue'
+import RandomQuoteGenerator from './components/RandomQuoteGenerator.vue'
+import AmazingCalculator from './components/AmazingCalculator.vue'
+import AccordionComponent from './components/AccordionComponent.vue'
+import FormValidation from './components/FormValidation.vue'
+import PasswordGenerator from './components/PasswordGenerator.vue'
+import ProgressSteps from './components/ProgressSteps.vue'
+import DadJokes from './components/DadJokes.vue'
+import GithubUserSearch from './components/GithubUserSearch.vue'
+import WikiClone from './components/WikiClone.vue'
+</script>
+
+<template>
+  <TodoList />
+  <RandomQuoteGenerator />
+  <AmazingCalculator />
+  <AccordionComponent />
+  <FormValidation />
+  <PasswordGenerator />
+  <ProgressSteps />
+  <DadJokes />
+  <GithubUserSearch />
+  <WikiClone />
+</template>
+
+<style scoped></style>
